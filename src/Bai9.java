67e1ea8308c0ba0c04ce551ffa0701798c1a4ed7
thecuongk5hp <147390984+thecuongk5hp@users.noreply.github.com>
2025-03-10 08:03:19 +0700

    Update Bai9.java

--- src/Bai9.java
+++ src/Bai9.java
@@ -1,51 +1,58 @@
 import java.util.Scanner;
-
-public class Bai9 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Nhập kích thước ma trận n: ");
-        int n = scanner.nextInt();
-
-        if (n <= 0) {
-            System.out.println("Ma trận không hợp lệ");
-            return;
-        }
-
-        int[][] matrix = new int[n][n];
-
-        System.out.println("Nhập các phần tử của ma trận:");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                matrix[i][j] = scanner.nextInt();
-            }
-        }
-
-        //Chuyển vị ma trận (hoán đổi hàng thành cột)
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
-            }
-        }
-
-        //Đảo ngược từng cột để hoàn thành việc xoay 90 độ
-        for (int i = 0; i < n; i++) {
-            for (int j = 0, k = n - 1; j < k; j++, k--) {
-                int temp = matrix[j][i];
-                matrix[j][i] = matrix[k][i];
-                matrix[k][i] = temp;
-            }
-        }
-
-        // In ma trận sau khi xoay
-        System.out.println("Ma trận sau khi xoay 90 độ:");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(matrix[i][j] + "\t");
-            }
-            System.out.println();
-        }
-    }
-}
+ 
+ public class Bai9 {
+     public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+ 
+         int n;
+         do {
+             System.out.print("Nhập số lượng phần tử của mảng: ");
+             n = sc.nextInt();
+             if (n <= 0) {
+                 System.out.println("Moi Nhập lại!");
+             }
+         } while (n <= 0);
+ 
+         int[][] matrix = new int[n][n];
+ 
+         System.out.println("Nhập các phần tử của ma trận:");
+         for (int i = 0; i < n; i++) {
+             for (int j = 0; j < n; j++) {
+                 matrix[i][j] = sc.nextInt();
+             }
+         }
+ 
+         System.out.println("\nMa trận ban đầu:");
+         for (int i = 0; i < n; i++) {
+             for (int j = 0; j < n; j++) {
+                 System.out.print(matrix[i][j] + "\t");
+             }
+             System.out.println();
+         }
+ 
+         for (int i = 0; i < n; i++) {
+             for (int j = i + 1; j < n; j++) {
+                 int temp = matrix[i][j];
+                 matrix[i][j] = matrix[j][i];
+                 matrix[j][i] = temp;
+             }
+         }
+ 
+         for (int i = 0; i < n; i++) {
+             for (int j = 0, k = n - 1; j < k; j++, k--) {
+                 int temp = matrix[i][j];
+                 matrix[i][j] = matrix[i][k];
+                 matrix[i][k] = temp;
+             }
+         }
+ 
+         System.out.println("\nMa trận sau khi xoay 90 độ:");
+         for (int i = 0; i < n; i++) {
+             for (int j = 0; j < n; j++) {
+                 System.out.print(matrix[i][j] + "\t");
+             }
+             System.out.println();
+         }
+         sc.close();
+     }
+ }
